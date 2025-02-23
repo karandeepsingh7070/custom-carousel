500b491da1b060713dc86d6c36cfa1dc36469855
karandeepsingh7070 <soundportrait2530@gmail.com>
2025-02-23 09:59:19 +0530

    script no loading fix

--- touchExperimental.js
+++ touchExperimental.js
@@ -1,11 +1,18 @@
-import { handleBlurryImages, renderImages } from "./loadImages.js";
+import { handleBlurryImages, renderImages } from "./loadImagesExperimental.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (event) => {
+    // const allImagesWrapper = document.querySelectorAll('.load-blury-bg')
     const carouselWrapper = document.querySelector('.carousel-wrapper')
     const toggleSwitch = document.querySelector(".toggle-switch input");
     let backNavigation = document.querySelector(".back");
     let nextNavigation = document.querySelector(".next");
+    let isTouchActive = false
+    let prevPageX
+    let prevScrollLeft
+    let scrollCoardinate
+    let hold = false
     let keyClickDebounce // to handle multiple key presses
+    // let isScrollDisabled = false
 
     const loadNextImages = () => {
         let index = JSON.parse(localStorage.getItem('index'))
@@ -74,6 +81,68 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    //
+    const snapToGrid = () => {
+        if (carouselWrapper.scrollLeft == (carouselWrapper.scrollWidth - carouselWrapper.clientWidth)) return
+        scrollCoardinate = Math.abs(scrollCoardinate)
+        let ImageWidth = 400
+        let totalShift = ImageWidth - scrollCoardinate
+        if (carouselWrapper.scrollLeft > prevScrollLeft) { // right scroll
+            if (scrollCoardinate > ImageWidth / 3) {
+                carouselWrapper.scrollTo({
+                    left: carouselWrapper.scrollLeft + totalShift,
+                    behavior: "smooth",
+                });
+                // carouselWrapper.scrollLeft += totalShift
+            } else { // revert to original position
+                carouselWrapper.scrollTo({
+                    left: carouselWrapper.scrollLeft - scrollCoardinate,
+                    behavior: "smooth",
+                });
+                // carouselWrapper.scrollLeft -= scrollCoardinate
+            }
+        } else { // left scroll
+            if (scrollCoardinate > ImageWidth / 3) {
+                carouselWrapper.scrollTo({
+                    left: carouselWrapper.scrollLeft - totalShift,
+                    behavior: "smooth",
+                });
+                // carouselWrapper.scrollLeft -= totalShift
+            } else { // revert to original position
+                carouselWrapper.scrollTo({
+                    left: carouselWrapper.scrollLeft + scrollCoardinate,
+                    behavior: "smooth",
+                });
+                // carouselWrapper.scrollLeft += scrollCoardinate
+            }
+        }
+    }
+    const trackPosition = (e) => {
+        if (!isTouchActive) return
+        e.preventDefault()
+        isScrollDisabled = true
+        hold = true
+        // console.log(e.pageX,prevPageX)
+        scrollCoardinate = (e.pageX || e.touches[0].pageX) - prevPageX
+        carouselWrapper.scrollLeft = prevScrollLeft - scrollCoardinate
+    }
+    const startTouch = (e) => {
+        e.preventDefault()
+        prevPageX = e.pageX || e.touches[0].pageX
+        prevScrollLeft = carouselWrapper.scrollLeft
+        isTouchActive = true
+        clearInterval(slideInterval)
+        resetTimer()
+    }
+    const endTouch = (e) => {
+        e.preventDefault()
+        isTouchActive = false
+        if (!hold) return
+        hold = false
+        isScrollDisabled = false
+        snapToGrid()
+    }
+
     const moveToRight = autoSlide
     const moveToLeft = () => {
         loadPreviousImages()
@@ -97,7 +166,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const attachEvents = () => {
+        carouselWrapper?.addEventListener('mousedown', startTouch)
+        carouselWrapper?.addEventListener('mousemove', trackPosition)
+        carouselWrapper?.addEventListener('mouseout', endTouch)
+        carouselWrapper?.addEventListener('mouseup', endTouch)
+
+        // carouselWrapper?.addEventListener('scroll', () => {
+        //     // infinite scroll
+        //     if (carouselWrapper.scrollLeft == (carouselWrapper.scrollWidth - carouselWrapper.clientWidth)) {
+        //         carouselWrapper.scrollTo({
+        //             left: 0,
+        //             behavior: "smooth",
+        //         });
+        //     }
+        // })
+        // restricting scroll behiviour
+        // carouselWrapper?.addEventListener('scroll', (e) => {
+        //     if (!isScrollDisabled) {
+        //         var scrollLeft = carouselWrapper.scrollLeft
+        //         let remainingShift = Math.ceil(scrollLeft / 400)
+        //         // console.log(scrollLeft, remainingShift)
+        //         carouselWrapper.scrollLeft = (remainingShift * 400) - scrollCoardinate
+        //     }
+        // })
+
+        carouselWrapper?.addEventListener('touchstart', startTouch)
+        carouselWrapper?.addEventListener('touchmove', trackPosition)
+        carouselWrapper?.addEventListener('touchcancel', endTouch)
+        carouselWrapper?.addEventListener('touchend', endTouch)
         document.addEventListener('keydown', handleKeyDown);
+
         backNavigation.addEventListener('click', moveToLeft)
         nextNavigation.addEventListener('click', moveToRight)
     }
